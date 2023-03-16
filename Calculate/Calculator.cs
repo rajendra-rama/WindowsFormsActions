9de84921ac4ab26ebfe516aa1a6e86c0f00f3882
Rajendra Rama <askforrajendra@outlook.com>
2023-03-16 16:50:38 +0000

    initial commit

---
+++ Calculate/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
